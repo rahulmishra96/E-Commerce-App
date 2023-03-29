8fc9ff2cc7f4ff2a81e992cfa2030e02d6568dd9
Rahul Mishra <rahulmishra9546@gmail.com>
2023-03-29 14:41:03 +0530

    front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import Home from "../src/pages/Home";
+// import React from "react";
+// import Product from "./pages/Product";
+// import Home from "../src/pages/Home";
+// import ProductList from "./pages/ProductList";
+// import Register from "../src/pages/Register";
+// import Login from "./pages/Login";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <>
-      <Home />
+      <Cart />
     </>
   );
 }
